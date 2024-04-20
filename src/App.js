bb68fc78e588748d550626625780f5e1a4527d3f
devgirlpro <agalidari@gmail.com>
2024-04-20 14:41:52 +0200

    fetch data and display the first random user name and image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
 import React from "react";
 
 function App() {
   const [user, setUser] = React.useState([]);
 
   const fetchData = () => {
-
+    fetch("https://randomuser.me/api/?results=1")
+      .then((response) => response.json())
+      .then((data) => setUser(data));
   };
 
   React.useEffect(() => {
@@ -13,9 +14,10 @@ function App() {
   }, []);
 
   return Object.keys(user).length > 0 ? (
-    <div>
+    <div style={{padding: "40px"}}>
       <h1>Customer data</h1>
-
+      <h2>Name: {user.results[0].name.first}</h2>
+      <img src={user.results[0].picture.large} alt="" />
     </div>
   ) : (
     <h1>Data pending...</h1>
